4f7a4ab362d730e19c62ca52f4d3a1d7497f92bf
Matthew Walton <mattwalt@gmail.com>
2026-03-06 10:05:02 -0500

    New pro_custom.

---
+++ misc/pro_custom.js
@@ -0,0 +1,27 @@
+
+black       = '#000000';
+red         = '#990000';
+green       = '#00a600';
+yellow      = '#999900';
+blue        = '#2009db';
+magenta     = '#b200b2';
+cyan        = '#00a6b2';
+white       = '#bfbfbf';
+lightBlack  = '#666666';
+lightRed    = '#e50000';
+lightGreen  = '#00d900';
+lightYellow = '#b3b300';
+lightBlue   = '#0000ff';
+lightMagenta= '#e500e5';
+lightCyan   = '#00e5e5';
+lightWhite  = '#e5e5e5';
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', green)');
+t.prefs_.set('foreground-color', lightWhite);
+t.prefs_.set('background-color', black);
