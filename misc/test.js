cd499555513c349f99afdaa710823661d0a9113a
Matthew Walton <mattwalt@gmail.com>
2026-03-06 09:21:05 -0500

    Addtest test.js and formatting.

--- misc/test.js
+++ misc/test.js
@@ -1,6 +1,6 @@
 t.prefs_.set('color-palette-overrides',["#000000", "#990000", "#00a600",
 "#999900", "#2009db", "#b200b2", "#00a6b2", "#bfbfbf", "#666666",
-"#e50000", "#00d900", "#b3b300", "#0000ff", "#e500e5", "#00e5e5",
+"#ffffff", "#00d900", "#b3b300", "#0000ff", "#e500e5", "#00e5e5",
 "#e5e5e5"]);
 t.prefs_.set('foreground-color', "#f2f2f2");
 t.prefs_.set('background-color', "#000000");
