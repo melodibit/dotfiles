56481e758b121025882a6cb3eb999c48cb958a0d
Matthew Walton <mattwalt@gmail.com>
2026-03-06 10:35:11 -0500

    Color update

--- misc/pro_custom.js
+++ misc/pro_custom.js
@@ -1,23 +1,23 @@
 //Color Picker: https://www.w3schools.com/colors/colors_picker.asp
 
-black       = '#000000';
-red         = '#990000';
-green       = '#00a600';
-yellow      = '#999900';
-blue        = '#3b23f6';
-magenta     = '#b200b2';
-cyan        = '#00a6b2';
-white       = '#bfbfbf';
-lightBlack  = '#666666';
-lightRed    = '#e50000';
-lightGreen  = '#00d900';
-lightYellow = '#b3b300';
-lightBlue   = '#0000ff';
-lightMagenta= '#e500e5';
-lightCyan   = '#00e5e5';
-lightWhite  = '#e5e5e5';
+black        = '#000000';
+red          = '#990000';
+green        = '#00a600';
+yellow       = '#999900';
+blue         = '#00aaff';
+magenta      = '#b200b2';
+cyan         = '#00a6b2';
+white        = '#bfbfbf';
+lightBlack   = '#666666';
+lightRed     = '#e50000';
+lightGreen   = '#00d900';
+lightYellow  = '#b3b300';
+lightBlue    = '#66ccff';
+lightMagenta = '#e500e5';
+lightCyan    = '#00e5e5';
+lightWhite   = '#e5e5e5';
 
-lime        = '#00ff00';
+lime         = '#00ff00';
 
 t.prefs_.set('color-palette-overrides',
                  [ black , red     , green  , yellow,
